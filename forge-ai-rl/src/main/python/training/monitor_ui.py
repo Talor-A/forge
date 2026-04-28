4994736af1753f3872111e0c25461feb4dcf337a
Talor <public-github@taloranderson.com>
2026-04-27 18:50:15 -0700

    monitor_ui: widen x-axis padding from rounds+3 to rounds+10

--- forge-ai-rl/src/main/python/training/monitor_ui.py
+++ forge-ai-rl/src/main/python/training/monitor_ui.py
@@ -174,7 +174,7 @@ class MonitorUI:
             label=f'best {best_wr:.1%}', zorder=2)
         self.ax_wr.plot(eval_rounds, win_rates, color='#a6e3a1',
             linewidth=2, marker='o', markersize=4, zorder=3)
-        self.ax_wr.set_xlim(1, n_rounds + 3)
+        self.ax_wr.set_xlim(1, n_rounds + 10)
         self.ax_wr.set_ylim(0, 0.6)
         self.ax_wr.set_ylabel('Win Rate',
             color='#a6adc8', fontsize=9)
@@ -203,7 +203,7 @@ class MonitorUI:
             ax2.set_ylabel('Entropy', color='#a6e3a1', fontsize=8)
             ax2.tick_params(colors='#6c7086', labelsize=7)
             ax2.spines['right'].set_color('#45475a')
-        self.ax_loss.set_xlim(1, n_rounds + 3)
+        self.ax_loss.set_xlim(1, n_rounds + 10)
         self.ax_loss.autoscale(axis='y')
         self.ax_loss.legend(fontsize=8, facecolor='#313244',
             edgecolor='#45475a', labelcolor='#cdd6f4')
@@ -231,7 +231,7 @@ class MonitorUI:
             self.ax_gp2.plot(gp_rounds[:len(spells)], spells,
                 color='#a6e3a1', linewidth=1.5,
                 label='Spells/Turn', marker='^', markersize=3)
-        self.ax_gp.set_xlim(1, n_rounds + 3)
+        self.ax_gp.set_xlim(1, n_rounds + 10)
         self.ax_gp.autoscale(axis='y')
         self.ax_gp2.autoscale(axis='y')
         self.ax_gp.set_ylabel('Percent',
